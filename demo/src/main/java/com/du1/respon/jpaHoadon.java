d73e77b3f5f249416eb633c9a566b5d59c489926
quyetdv28978 <107168139+quyetdv28978@users.noreply.github.com>
2023-10-05 15:06:59 +0700

    update

--- demo/src/main/java/com/du1/respon/jpaHoadon.java
+++
@@ -1,9 +0,0 @@
-package com.du1.respon;
-
-import com.du1.model.entity.Hoadon;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface jpaHoadon extends JpaRepository<Hoadon, Integer> {
-}
--- demo/src/main/java/com/du1/respon/jpaVaiTro.java
+++
@@ -1,6 +0,0 @@
-package com.du1.respon;
-
-import com.du1.model.entity.vaitro;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface jpaVaiTro extends JpaRepository<vaitro, Integer> { }
