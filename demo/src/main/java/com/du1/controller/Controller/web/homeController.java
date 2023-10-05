9ff891a40c3166a6a85cf061895832ed64b0327a
quyetdv28978 <107168139+quyetdv28978@users.noreply.github.com>
2023-10-05 15:09:00 +0700

    update

--- demo/src/main/java/com/du1/controller/Controller/web/homeController.java
+++ demo/src/main/java/com/du1/controller/Controller/web/homeController.java
@@ -1,6 +1,5 @@
 package com.du1.controller.Controller.web;
 
-import com.du1.respon.JpaUsers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,12 +9,12 @@ import java.security.Principal;
 @Controller
 public class homeController {
 
-    @Autowired
-    private JpaUsers jpaUsers;
+//    @Autowired
+//    private JpaUsers jpaUsers;
 
     @GetMapping("home")
     public String home(Model model, Principal principal){
-        model.addAttribute("users", usersController.jwtUser);
+//        model.addAttribute("users", usersController.jwtUser);
         return "web/index.html";
     }
 }
