699f21f2ee0e1bb3d85cf040a9cb2ad2efdfa252
quyetdv28978 <107168139+quyetdv28978@users.noreply.github.com>
2023-10-05 15:00:36 +0700

    update

--- demo/src/main/java/com/du1/DemoApplication.java
+++ demo/src/main/java/com/du1/DemoApplication.java
@@ -2,14 +2,16 @@ package com.du1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 import java.text.ParseException;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class DemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) throws ParseException {
-      SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(DemoApplication.class, args);
     }
 
 }
